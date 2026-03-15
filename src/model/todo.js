f1d18af034ef92e926281896b77081d6d54153b2
gr-pf <237513179+gr-pf@users.noreply.github.com>
2026-03-15 12:26:11 +0100

    Add data validation to class

--- src/model/todo.js
+++ src/model/todo.js
@@ -1,13 +1,42 @@
+// file : src/model/todo.js
+
+const PRIORITY = ["high", "medium", "low", null];
+
 export class Todo {
   /**
    *
    * @param {string} name
-   * @param {string} content
-   * @param {Date} date
-   * @param {string} priority
-   * @param {Array<string>} tags
+   * @param {string|undefined} content
+   * @param {Date|undefined} date
+   * @param {?string} priority
+   * @param {Array.<string>|string|undefined} tags
    */
-  constructor(name, content, date, priority, tags) {
+  constructor(name, content, date, priority = null, tags) {
+    if (typeof name !== "string") {
+      throw new TypeError(
+        `name doit être une chaine valide, reçu : ${name} (${typeof name})`,
+      );
+    }
+    if (!/^[\p{L}\s\-]+$/u.test(name)) {
+      throw new SyntaxError(
+        `name doit être une chaine valide de format [a-zA-Z], reçu : ${name}`,
+      );
+    }
+    if (content !== undefined && typeof content !== "string") {
+      throw new TypeError(
+        `content doit être une chaîne, reçu : ${content} (${typeof content})`,
+      );
+    }
+    if (date !== undefined && !(date instanceof Date)) {
+      throw new TypeError(
+        `date doit être une date valide, reçu : ${date} (${typeof date})`,
+      );
+    }
+    if (!PRIORITY.includes(priority)) {
+      throw new TypeError(
+        `priority doit être dans la liste des valeurs "high", "medium", "low", null, reçu : ${priority}`,
+      );
+    }
     this.name = name;
     this.content = content;
     this.date = date;
