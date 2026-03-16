547ea4fcad42c1ff6e9791cdc57825d39edf26a2
gr-pf <237513179+gr-pf@users.noreply.github.com>
2026-03-16 18:22:00 +0100

    Clean file

--- src/model/todo-list.js
+++ src/model/todo-list.js
@@ -81,22 +81,3 @@ export class TodoList {
     }, this);
   }
 }
-
-const todo01 = new Todo({ name: "todo-one1", tags: "work" });
-const todo02 = new Todo({ name: "todo-two", tags: ["cine", "tv"] });
-const todo03 = new Todo({ name: "todo-three", tags: ["food", "tv"] });
-
-const newList = new TodoList();
-
-console.log(newList.tags);
-newList.addTodo(todo01);
-console.log(newList.tags);
-newList.addTodo(todo02);
-newList.addTodo(todo03);
-console.log(newList.tags);
-newList.removeTodo(todo02);
-newList.removeTodo(todo02);
-newList.addTodo(todo01);
-console.log(newList.todoList);
-console.log(newList.tags);
-console.log(newList.todoList.values().next().value.name);
