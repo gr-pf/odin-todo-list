d712bb72575fa4d4a6847b7bf25dac8176ea516f
gr-pf <237513179+gr-pf@users.noreply.github.com>
2026-03-16 18:22:25 +0100

    Update import and add tests

--- src/index.js
+++ src/index.js
@@ -1,4 +1,24 @@
-import { TODO_LIST } from "./model/todo-list.js";
+import { TodoList } from "./model/todo-list.js";
 import { Todo } from "./model/todo.js";
 
+const TODO_LIST = new TodoList();
 window.TODO_LIST = TODO_LIST;
+
+const todo01 = new Todo({ name: "todo-one1", tags: "work" });
+const todo02 = new Todo({ name: "todo-two", tags: ["cine", "tv"] });
+const todo03 = new Todo({ name: "todo-three", tags: ["food", "tv"] });
+
+const newList = new TodoList();
+
+console.log(newList.tags);
+newList.addTodo(todo01);
+console.log(newList.tags);
+newList.addTodo(todo02);
+newList.addTodo(todo03);
+console.log(newList.tags);
+newList.removeTodo(todo02);
+newList.removeTodo(todo02);
+newList.addTodo(todo01);
+console.log(newList.todoList);
+console.log(newList.tags);
+console.log(newList.todoList.values().next().value.name);
