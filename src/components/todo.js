6f2a5976628a240aa3e3f4bff2804b89a8fd8c6e
gr-pf <237513179+gr-pf@users.noreply.github.com>
2026-03-29 15:53:54 +0200

    Add renderTodo comments

--- src/components/todo.js
+++ src/components/todo.js
@@ -2,6 +2,8 @@
 
 import { Todo } from "./model/todo.js";
 
+// Implémentation à terminer
+
 /**
  *
  * @param {Todo} todo
@@ -11,10 +13,15 @@ export function renderTodo(todo) {
     throw new TypeError("todo doit être une instance de Todo");
   }
   const liTodo = document.createElement("li");
+
+  // On utilise .innerHTML pour gérer la structuration du DOM
   liTodo.innerHTML = `
             <h3 class="todo-name"></h3>
             <p class="todo-content"></p>
         `;
+
+  // On utilise .textContent pour intégrer de manière safe
+  // les inputs utilisateur
   liTodo.querySelector(".todo-name").textContent = todo.name;
   liTodo.querySelector(".todo-content").textContent = todo.content ?? "";
 
