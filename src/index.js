8c591b7da8e572381ba8b19f9692e34aaca624e2
gr-pf <237513179+gr-pf@users.noreply.github.com>
2026-03-29 17:02:25 +0200

    Testing renderTodo

--- src/index.js
+++ src/index.js
@@ -1,16 +1,22 @@
 import { TodoList } from "./model/todo-list.js";
 import { Todo } from "./model/todo.js";
+import { renderTodo } from "./components/todo.js";
 import "./style.css";
 
 const TODO_LIST = new TodoList();
 window.TODO_LIST = TODO_LIST;
 
-const todo01 = new Todo({ name: "todo-one1", tags: "work" });
+const todo01 = new Todo({
+  name: "todo-one1",
+  content: "Ceci est un contenu test",
+  tags: "work",
+});
 const todo02 = new Todo({ name: "todo-two", tags: ["cine", "tv"] });
 const todo03 = new Todo({ name: "todo-three", tags: ["food", "tv"] });
 
 const newList = new TodoList();
 
+console.log(newList);
 console.log(newList.tags);
 newList.addTodo(todo01);
 console.log(newList.tags);
@@ -23,3 +29,9 @@ newList.addTodo(todo01);
 console.log(newList.todoList);
 console.log(newList.tags);
 console.log(newList.todoList.values().next().value.name);
+console.log(newList);
+
+const ulTodoList = document.querySelector(".todo-list");
+newList.todoList.forEach((todo, id) =>
+  ulTodoList.appendChild(renderTodo(todo)),
+);
