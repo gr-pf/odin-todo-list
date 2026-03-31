d35ea1024572a53247f6d919ec646ab6a7261157
gr-pf <237513179+gr-pf@users.noreply.github.com>
2026-03-31 19:46:47 +0200

    Init TodoForm component

---
+++ src/components/add-todo-form.js
@@ -0,0 +1,8 @@
+// file : src/components/add-todo-form.js
+
+export function renderFormTodo() {
+  const liFormTodo = document.createElement("div");
+  liFormTodo.classList = "form-todo-wrapper";
+
+  return liFormTodo;
+}
