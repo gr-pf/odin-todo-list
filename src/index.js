15731f83f52b1b00ae8cd121b3a73995619b8f2e
gr-pf <237513179+gr-pf@users.noreply.github.com>
2026-04-05 16:01:59 +0200

    Update for singleton

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import { TodoList } from "./model/todo-list.js";
+import { TodoList, singletonTodoList } from "./model/todo-list.js";
 import { Todo } from "./model/todo.js";
 import { renderTodo } from "./components/todo.js";
 import { renderFormTodo } from "./components/add-todo-form.js";
@@ -6,7 +6,7 @@ import { callForm, callTodoList } from "./controllers/todo.js";
 import { renderTodoList } from "./components/todo-list.js";
 import "./style.css";
 
-const TODO_LIST = new TodoList();
+const TODO_LIST = singletonTodoList;
 window.TODO_LIST = TODO_LIST;
 
 const todo01 = new Todo({
@@ -19,31 +19,29 @@ const todo01 = new Todo({
 const todo02 = new Todo({ name: "todo-two", tags: ["cine", "tv"] });
 const todo03 = new Todo({ name: "todo-three", tags: ["food", "tv"] });
 
-const newList = new TodoList();
-
-console.log(newList);
-console.log(newList.tags);
-newList.addTodo(todo01);
-console.log(newList.tags);
-newList.addTodo(todo02);
-newList.addTodo(todo03);
-console.log(newList.tags);
-newList.removeTodo(todo02);
-newList.removeTodo(todo02);
-newList.addTodo(todo01);
-console.log(newList.todoList);
-console.log(newList.tags);
-console.log(newList.todoList.values().next().value.name);
+console.log(singletonTodoList);
+console.log(singletonTodoList.tags);
+singletonTodoList.addTodo(todo01);
+console.log(singletonTodoList.tags);
+singletonTodoList.addTodo(todo02);
+singletonTodoList.addTodo(todo03);
+console.log(singletonTodoList.tags);
+singletonTodoList.removeTodo(todo02);
+singletonTodoList.removeTodo(todo02);
+singletonTodoList.addTodo(todo01);
+console.log(singletonTodoList.todoList);
+console.log(singletonTodoList.tags);
+console.log(singletonTodoList.todoList.values().next().value.name);
 console.log("todolist:");
-console.log(newList);
+console.log(singletonTodoList);
 
 const ulTodoList = document.querySelector(".todo-list");
 const pseudoTodo = document.querySelector("#anchor-todolist");
-// newList.todoList.forEach((todo, id) =>
+// singletonTodoList.todoList.forEach((todo, id) =>
 //   ulTodoList.appendChild(renderTodo(todo)),
 // );
 
-//renderTodoList(newList).forEach((todo) => ulTodoList.append(todo));
-console.log(renderTodoList(newList));
-callForm(newList, "add-todo", "main");
-callTodoList(newList);
+//renderTodoList(singletonTodoList).forEach((todo) => ulTodoList.append(todo));
+console.log(renderTodoList(singletonTodoList));
+callForm(singletonTodoList, "add-todo", "main");
+callTodoList(singletonTodoList);
