f2b6fc52101ecde75ba3d991cadbf56d906cfdf3
gr-pf <237513179+gr-pf@users.noreply.github.com>
2026-04-11 17:30:22 +0200

    Move to controllers

---
+++ src/controllers/ui-state.js
@@ -0,0 +1,14 @@
+// file : src/components/ui-state.js
+
+export const uiState = {
+  tag: null,
+  date: null,
+  priority: null,
+  sortBy: {
+    active: false,
+    desc: false,
+    tag: false,
+    date: false,
+    priority: false,
+  },
+};
