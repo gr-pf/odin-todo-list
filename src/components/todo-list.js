076e53bf85b0fce80c3d97cc59acea58126881b0
gr-pf <237513179+gr-pf@users.noreply.github.com>
2026-04-11 17:27:27 +0200

    Extract logc filterTodoList from renderTodoList

--- src/components/todo-list.js
+++ src/components/todo-list.js
@@ -1,25 +1,45 @@
 // file : src/components/todo-list.js
 
-import { renderTodo } from "../components/todo.js";
+import { renderTodo } from "./todo.js";
 import { TodoList } from "../model/todo-list.js";
+import { uiState } from "./ui-state.js";
 
 /**
  *
  * @param {TodoList} todolist
- * @param {string} tag
+ * @param {uiState} state
  */
-export function renderTodoList(todolist, tag = null) {
+export function renderTodoList(todolist, state = uiState) {
   const listOfNode = [];
-  let currentList = [...todolist.todoList.values()];
+  let todosToRender = filterTodoList(todolist, (state = uiState));
 
-  if (tag) {
-    currentList = [...todolist.todoList.values()].filter((todo) =>
-      todo.tags.has(tag),
-    );
-  }
-  currentList.forEach((todo) => {
+  todosToRender.forEach((todo) => {
     listOfNode.push(renderTodo(todo));
   });
 
   return listOfNode;
+}
+
+/**
+ * Fonction pour filter les todos à affhicer en fonction
+ * du state de l'UI
+ * @param {TodoList} todolist
+ * @param {uiState} state
+ */
+function filterTodoList(todolist, state = uiState) {
+  let todosToRender = Array.from(todolist.todoList.values());
+  if (state.tag) {
+    todosToRender = todosToRender.filter((todo) => todo.tags.has(state.tag));
+  }
+  if (state.date) {
+    todosToRender = todosToRender.filter(
+      (todo) => todo.date.getTime() === state.date.getTime(),
+    );
+  }
+  if (state.priority) {
+    todosToRender = todosToRender.filter(
+      (todo) => todo.priority === state.priority,
+    );
+  }
+  return todosToRender;
 }
