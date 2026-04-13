41b2396eb53351db259dad0c281a1fb0e6b69b02
gr-pf <237513179+gr-pf@users.noreply.github.com>
2026-04-13 14:06:07 +0200

    Fix loadStorage

--- src/init.js
+++ src/init.js
@@ -1,9 +1,28 @@
 // file : src/init.js
 
-import { singletonTodoList } from "./model/todo-list.js";
+import { TodoList, singletonTodoList } from "./model/todo-list.js";
+import { Todo } from "./model/todo.js";
 import { callForm, callTodoList } from "./controllers/todo.js";
 
 export function initApp() {
+  loadStorage(singletonTodoList);
   callForm(singletonTodoList, "add-todo", "main");
   callTodoList(singletonTodoList);
+}
+
+/**
+ * Charge les données depuis le localstorage
+ * @param {TodoList} todolist
+ */
+function loadStorage(todolist = singletonTodoList) {
+  if (localStorage.getItem("todolist")) {
+    const objTodoList = JSON.parse(localStorage.getItem("todolist"));
+    Object.values(objTodoList).forEach(function (todo) {
+      if (todo.date) {
+        todo.date = new Date(todo.date);
+      }
+      const newTodo = new Todo(todo);
+      todolist.addTodo(newTodo);
+    });
+  }
 }
