4d812a3aaa19c1ceba075b29926c7f26de4085a9
gr-pf <237513179+gr-pf@users.noreply.github.com>
2026-04-13 14:09:47 +0200

    Update index

--- src/index.js
+++ src/index.js
@@ -1,48 +1,15 @@
+// file : src/index.js
+
 import { TodoList, singletonTodoList } from "./model/todo-list.js";
 import { Todo } from "./model/todo.js";
-import { renderTodo } from "./components/todo.js";
-import { renderFormTodo } from "./components/add-todo-form.js";
-import { renderTodoList } from "./components/todo-list.js";
-import { callForm, callTodoList, callTag } from "./controllers/todo.js";
-import { uiState } from "./controllers/ui-state.js";
 import { randomTodos } from "./tests/generate-todo.js";
+import { initApp } from "./init.js";
 import "./style.css";
 
-const TODO_LIST = singletonTodoList;
-window.TODO_LIST = TODO_LIST;
-const globalState = uiState;
-window.globalState = globalState;
+const objstr = `{"70e862b5-f020-4171-8227-3948b9400002":{"name":"todo-one1","content":"Ceci est un contenu test","date":"2026-03-24T23:00:00.000Z","priority":"medium","tags":["cine","tv","work"]},"a7e882e3-bda9-4a76-8778-2cbf04e6a486":{"name":"todo-two","priority":null,"tags":["cine","tv"]},"c01f0d12-6212-46fa-a063-0c667857b420":{"name":"todo-three","priority":null,"tags":["food","tv"]},"8fbbd479-7bf2-4708-b37f-adc781815362":{"name":"todo-three","priority":null}}`;
 
-const todo01 = new Todo({
-  name: "todo-one1",
-  priority: "medium",
-  date: new Date(2026, 2, 25),
-  content: "Ceci est un contenu test",
-  tags: ["cine", "tv", "work"],
-});
-const todo02 = new Todo({ name: "todo-two", tags: ["cine", "tv"] });
-const todo03 = new Todo({ name: "todo-three", tags: ["food", "tv"] });
-const todo04 = new Todo({ name: "todo-three", tags: null });
+localStorage.setItem("todolist", objstr);
 
-console.log(singletonTodoList);
-console.log(singletonTodoList.tags);
-singletonTodoList.addTodo(todo01);
-console.log(singletonTodoList.tags);
-singletonTodoList.addTodo(todo02);
-singletonTodoList.addTodo(todo03);
-console.log(singletonTodoList.tags);
-singletonTodoList.removeTodo(todo02);
-singletonTodoList.removeTodo(todo02);
-singletonTodoList.addTodo(todo01);
-singletonTodoList.addTodo(todo04);
-console.log(singletonTodoList.todoList);
-console.log(singletonTodoList.tags);
-console.log(singletonTodoList.todoList.values().next().value.name);
-console.log("todolist:");
-console.log(singletonTodoList);
+initApp();
 
-randomTodos.forEach((todo) => singletonTodoList.addTodo(new Todo(todo)));
-
-console.log(renderTodoList(singletonTodoList));
-callForm(singletonTodoList, "add-todo", "main");
-callTodoList(singletonTodoList, undefined, undefined);
+//randomTodos.forEach((todo) => singletonTodoList.addTodo(new Todo(todo)));
